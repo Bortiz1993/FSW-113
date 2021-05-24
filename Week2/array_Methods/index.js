3ebfe6161b032b0b2556d177fc84ede2915ce5c7
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2021-05-23 19:13:25 -0500

    update

--- Week2/array_Methods/index.js
+++ Week2/array_Methods/index.js
@@ -20,3 +20,60 @@ const parts = [
     { partNbr: 'POO9ZPM', partDescr: 'Eumonklippen', aisle: 'D2', qty: 7},
     { partNbr: 'WEYPU3Z', partDescr: 'Mumpenflipper', aisle: 'E3', qty: 1}
 ]
+//First Problem: // use maybe for loop/ or use .Find method or .forEach.
+parts.forEach(function(list){
+    console.log(`qty: ${list.qty} partNumber: ${list.partNbr}`);
+});
+console.log("*******")
+
+//second problem:
+parts.forEach(function(list){
+    if (list.aisle === 'B3'){
+        console.log("Aisle: " + list.aisle + '; requires special packaging')
+    }
+    else {
+        console.log("Aisle: " + list.aisle + "; Does not require special packaging")
+    }
+});
+console.log("*******")
+//Third problem:
+parts.forEach(function(list){
+    if (list.partDescr === "Shreckendwammer" && list.aisle === 'J4'){
+        console.log("Part description: " + list.partDescr + " aisle: " + list.aisle + '; You will need a pair of gloves from the storeroom!')
+    }
+    else {
+        console.log("Part description: " + list.partDescr + " aisle: " + list.aisle + "; No gloves required")
+    }
+});
+console.log("******")
+
+//fourth problem:
+parts.some((list) => {
+    if (list.aisle === 'H1'){
+    console.log( "Part number: " + list.partNbr + "; You will need a basket in aisle H1!")
+
+}
+    else {
+        console.log("Part number: " + list.partNbr +  '; You don`t need a basket.')
+    }
+});
+console.log('******')
+
+//Fifth problem:
+parts.forEach(function(list){
+    if (list.aisle === 'S' || list.aisle ==='T' || list.aisle === 'U'){
+        console.log("Part description: " + list.partDescr + "; You will need a Forklift")
+    }
+    else {
+        console.log("Part description: " + list.partDescr + "; No forklift required")
+    }
+});
+console.log("*****")
+
+//Sixth problem:
+let qtySum  = parts.reduce((total, quantity) =>  total + quantity.qty ,0);
+console.log("Final tally of items: " + qtySum);
+
+//Final problem:
+let newArray = parts.map(part => "Part number: " + part.partNbr + ' Aisle: ' +  part.aisle);
+console.log(newArray)

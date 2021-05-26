048883f6687db2636b76462e8b90721f46d375a9
Bortiz1993 <brijido.zuniga@bryanuniversity.edu>
2021-05-26 17:35:11 -0500

    update2

--- Week2/arrow_Functions/index.js
+++ Week2/arrow_Functions/index.js
@@ -1,14 +1,14 @@
 const aryGrades = [66,72,100,88,92,74,72,94,80,78]
 
-let sum = (accumulator, currentValue) => accumulator + currentValue;
-
-let sumGrades = array => array.reduce(sum); 
+let sumGrades = array => array.reduce((accumulator, currentValue) => {return accumulator + currentValue}); 
 
 let minGrade = aryGrades.reduce((a, b) => { return Math.min(a, b)})
 
 let maxGrade = aryGrades.reduce((a, b) => { return Math.max(a, b)})
 
-var mean = sumGrades(aryGrades) / aryGrades.length, gradeSlice = (maxGrade - minGrade) / 5;
+let mean = sumGrades(aryGrades) / aryGrades.length
+
+let = gradeSlice = (maxGrade - minGrade) / 5;
 
 function applyBell(grade, index, ary) {
     switch (true) {
